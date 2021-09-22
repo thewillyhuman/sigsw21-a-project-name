a86c0c66bdfbe8541811d84286fe47b8c8dbbcb3
mistermboy <pabloyo97@hotmail.com>
2021-09-22 13:29:01 +0200

    responsive layout

--- src/client/src/components/PlanificatePage.js
+++ src/client/src/components/PlanificatePage.js
@@ -1,23 +1,52 @@
-import {Row,Col,Form,Button} from 'react-bootstrap';
+import {Row,Col,Container} from 'react-bootstrap';
 
 function PlanificatePage(){
     return(
-        <div className="planificate-body">
+        <Container  className="planificate-body">
             <Row>
-                <Col>
+                <Col md={3}>
                     <div className="days-panel"><p>Days Panel</p></div>
                 </Col>
-                <Col>
-                    <Row>
-                        <div className="info-day-panel"><p>Info Day Panel</p></div>
-                    </Row> 
-                    <Row>
-                        <div className="map-panel"><p>Map</p></div>
-                    </Row> 
+                <Col md>
+                    <div className="day-container">
+                        <Row>
+                            <Col>
+                                <div className="day-panel">
+                                    <p>Día 1</p>
+                                </div>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col>
+                                <div className="map-panel">
+                                    <p>Esto es un mapa</p>
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
                 </Col>
             </Row> 
-        </div>
+        </Container>
     )
 }
 
 export default PlanificatePage;
+
+/*
+ <Container  className="planificate-body">
+            <Row>
+                <Col md>
+                    <div className="days-panel"><p>Days Panel</p></div>
+                </Col>
+                <Col>
+                    <Container   className="planificate-body-col2">
+                        <Row>
+                            <div className="info-day-panel"><p>Info Day Panel</p></div>
+                        </Row> 
+                        <Row>
+                            <div className="map-panel"><p>Map</p></div>
+                        </Row> 
+                    </Container>
+                </Col>
+            </Row> 
+        </Container>*/
