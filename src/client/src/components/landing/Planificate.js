e5635bad73de33b64df01af8c61fbc388292f95b
mistermboy <pabloyo97@hotmail.com>
2021-09-24 17:05:16 +0200

    post

--- src/client/src/components/landing/Planificate.js
+++ src/client/src/components/landing/Planificate.js
@@ -1,6 +1,6 @@
 import { useContext,useState } from "react";
 import axios from 'axios';
-import {Row,Col,Form} from 'react-bootstrap';
+import {Row,Col,Form,Button} from 'react-bootstrap';
 import { scroller } from 'react-scroll';
 import {LandingContext} from '../LandingPage';
 
@@ -17,30 +17,46 @@ function Planificate(){
             days:days,
             km:km
         })
-
-        let bodyFormData = new FormData();
-        bodyFormData.set('transportMethod', context.transport);
-        bodyFormData.set('road_name', context.way);
-        bodyFormData.set('numberOfDays', context.days);
-
-        axios({
-            method: 'get',
-            url: 'http://santiagoapp.wcr.es:8080/routes',
-            data: bodyFormData,
-            config: { headers: {'Content-Type': 'application/json' }}
-        })
-        .then(function(response){
-            console.log(response)
-            scroller.scrollTo('route-visualizer', {
-                duration: 500,
-                delay: 0,
-                smooth: 'easeInOutQuart'
-            })
-        })
-        .catch(function (response) {
-            console.log('server error');
-            console.log(response);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        
+        var raw = JSON.stringify({
+          "road_name": "a",
+          "transportMethod": "pie",
+          "numberOfDays": 5
         });
+        
+        var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+        };
+        
+        fetch("http://santiagoapp.wcr.es:8080/routes", requestOptions)
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
+
+          var data = JSON.stringify({
+            "road_name": "a",
+            "transportMethod": "pie",
+            "numberOfDays": 5
+          });
+          
+          var xhr = new XMLHttpRequest();
+          xhr.withCredentials = true;
+          
+          xhr.addEventListener("readystatechange", function() {
+            if(this.readyState === 4) {
+             // console.log(this.responseText);
+            }
+          });
+          
+          xhr.open("POST", "http://santiagoapp.wcr.es:8080/routes");
+          xhr.setRequestHeader("Content-Type", "application/json");
+          
+          xhr.send(data);
 
         window.removeEventListener("resize", ()=>context.scrollToActive('route-visualizer'));
         window.addEventListener("resize",()=>context.scrollToActive('route-visualizer'));
@@ -61,22 +77,23 @@ function Planificate(){
 
 
     return(
-        <div className="landing-config-time">
+       <>
             <h1>Planifica tu viaje</h1>
+      
             <Row>
                 <Col>
                     <Form.Label>Días</Form.Label>
-                    <Form.Control size="lg" type="number" min="1" value={days} onChange={handleDays}/>
+                    <Form.Control size="lg" type="number" min="1" value={days} onChange={handleDays} required/>
                 </Col>
                 <Col>
                     <Form.Label>Km/Día</Form.Label>
-                    <Form.Control size="lg" type="number"  min="0.5" value={km} onChange={handleKm}/>
+                    <Form.Control size="lg" type="number"  min="0.5" value={km} onChange={handleKm} required/>
                 </Col>
             </Row>
             <Row>
-                <a className="planificatBtn" onClick={submit}>Planificar</a>
+                <button onClick={submit}className="planificatBtn">Planificar </button>
             </Row>
-        </div>
+            </>
     )
 }
 
