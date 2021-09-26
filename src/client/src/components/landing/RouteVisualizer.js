d71690d21fcb3d232797e75e96c2028f11a34fc7
mistermboy <pabloyo97@hotmail.com>
2021-09-27 01:26:13 +0200

    ... en pueblos

--- src/client/src/components/landing/RouteVisualizer.js
+++ src/client/src/components/landing/RouteVisualizer.js
@@ -59,7 +59,7 @@ function RouteVisualizer(){
                                                     <div className="place-name">
                                                         <ul>
                                                             {day?.route_locations?.map((place,i)=>{
-                                                                return (<li key={i}>{place?.split(',')[0]?.substring(6)}</li>)
+                                                                return (<li key={i}>{place?.split(',')[0]}</li>)
                                                             })}
                                                         </ul>
                                                     </div>
